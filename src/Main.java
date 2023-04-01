439fdf24be5918326b1a12f41abd02e3a50d2994
imransalg <imransalgiriev5200@gmail.com>
2023-04-01 11:39:46 +0300

    hw done)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         boolean result = Validator.validate("LogiN_123", "paSSworD_321", "paSSworD_322");
         if (result) {
             System.out.println("Логин и пароль корректные!");
